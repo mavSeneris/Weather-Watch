1bdfc28e9fccf53cbb3715be66c1376ef3b56931
Maverick <mavseneris@gmail.com>
2023-06-30 21:56:51 +0800

    api url added

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [weatherData, setWeatherData] = useState(null);
+
+  useEffect(() => {
+    fetchWeatherData();
+  }, []);
+
+  const fetchWeatherData = async () => {
+    try {
+      const apiKey = '74b102f2976c56f90984b6ccb90b1802';
+      const city = 'London';
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+      const response = await fetch(url);
+      const jsonData = await response.json();
+
+      setWeatherData(jsonData);
+    } catch (error) {
+      console.log('Error:', error);
+    }
+  };
+
+  if (!weatherData) {
+    return <div>Loading...</div>;
+  }
+
+  console.log(weatherData)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      {/* Render the weather data */}
+      <h2>{weatherData.name}</h2>
+      <p>Temperature: {weatherData.main.temp} K</p>
+      <p>Description: {weatherData.weather[0].description}</p>
+    </div>
+  );
 }
 
-export default App
+export default App;
