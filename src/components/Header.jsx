19c694d8f0cacf5e06609fc34a6c16c63986d73f
Maverick <mavseneris@gmail.com>
2023-07-03 09:52:49 +0800

    added active styles to navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,11 +2,31 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 export default function Header() {
+  const activeStyles = {
+    fontWeight: "bold",
+    color: "#1d71b8",
+  };
+
   return (
     <header>
-      <NavLink to="/">Today</NavLink>
-      <NavLink to="/hourly">Hourly</NavLink>
-      <NavLink to="/daily">Daily</NavLink>
+      <NavLink
+        style={({ isActive }) => (isActive ? activeStyles : null)}
+        to="/"
+      >
+        Today
+      </NavLink>
+      <NavLink
+        to="/hourly"
+        style={({ isActive }) => (isActive ? activeStyles : null)}
+      >
+        Hourly
+      </NavLink>
+      <NavLink
+        to="/daily"
+        style={({ isActive }) => (isActive ? activeStyles : null)}
+      >
+        Daily
+      </NavLink>
     </header>
   );
 }
